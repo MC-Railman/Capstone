6746d4be29c191e027975729abc7a141209b7a58
Tristan Hess <tlh173@zips.uakron.edu>
2020-04-23 01:39:15 -0400

    Add dessert files, dessert_fragment WIP

---
+++ app/src/main/java/com/example/capstone/ui/dessert/DessertViewModel.java
@@ -0,0 +1,19 @@
+package com.example.capstone.ui.dessert;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DessertViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DessertViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is the dessert fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
